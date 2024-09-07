c911c568874c8048bdcff19321da41534b9b0462
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-07 19:31:42 +0200

    add authorization endpoints to the server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT;
+
+app.use("/api", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
