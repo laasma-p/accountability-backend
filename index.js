3f7ccec6f5ea847440d84788e70da1a5389dce74
Lasma Poksane <laasmapoksane@gmail.com>
2024-09-10 11:22:47 +0200

    add habits-related endpoints to the server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
+const habitsRoutes = require("./routes/habits");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 const PORT = process.env.PORT;
 
 app.use("/api", authRoutes);
+app.use("/api", habitsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
